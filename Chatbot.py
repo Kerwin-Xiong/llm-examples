988237bece44bafdb2e988a9af4a90255011afe3
Kerwin-Xiong <814239928@qq.com>
2024-05-08 22:10:00 +0800

    Update Chatbot.py
    dashscope_api_key

--- Chatbot.py
+++ Chatbot.py
@@ -21,7 +21,7 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = tongyi(api_key=tongyi_api_key)
+    client = tongyi(dashscope_api_key=tongyi_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="qwen-turbo", messages=st.session_state.messages)
